50f9046175676871c90c2feb296269b9c68af6e3
gzlboy <332446332@qq.com>
2024-03-08 12:09:47 +0800

    Initial Commit

---
+++ SwiftUI-WebView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-WebView
+//
+//  Created by admin on 2024/3/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-WebView/SwiftUI_WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WebViewApp.swift
+//  SwiftUI-WebView
+//
+//  Created by admin on 2024/3/8.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
